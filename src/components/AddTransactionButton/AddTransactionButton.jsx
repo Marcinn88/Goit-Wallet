1c898eab88a91eb1daf80dd63ba5a1d3b12953d9
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-09-28 15:30:04 +0200

    pretty code

--- src/components/AddTransactionButton/AddTransactionButton.jsx
+++ src/components/AddTransactionButton/AddTransactionButton.jsx
@@ -1,91 +1,88 @@
-import React, {useState} from "react"
-import styles from "../AddTransactionButton/AddTransactionButton.module.css"
-import { ModalAddTransaction } from "../ModalAddTransaction/ModalAddTransaction"
-import { getDay, getMonth, getDefYear } from "../../services/DateFunctions"
-const dbURL = "https://cosmic-answer-399520.lm.r.appspot.com/api/mockTransactions/?month=&year="
+import React, { useState } from "react";
+import styles from "../AddTransactionButton/AddTransactionButton.module.css";
+import { ModalAddTransaction } from "../ModalAddTransaction/ModalAddTransaction";
+import { getDay, getMonth, getDefYear } from "../../services/DateFunctions";
+const dbURL =
+  "https://cosmic-answer-399520.lm.r.appspot.com/api/mockTransactions/?month=&year=";
 export const AddTransactionButton = (props) => {
+  const [modal, setModal] = useState(false);
+  const [data, setData] = useState();
 
-const [modal, setModal] = useState(false)
-const [data, setData] = useState()
-
-
-const dataFetch = async () => {
+  const dataFetch = async () => {
     const results = await (await fetch(dbURL)).json();
-    const data = results.data
+    const data = results.data;
     setData(data);
-    console.log('dane wczytane')
-};
+    console.log("dane wczytane");
+  };
 
-const dateTrim = (e) => {
-    const selectedData = e.target.value.toString()
-    const day = selectedData.substr(8,2)
-    const month = selectedData.substr(5,2)
-    const year = selectedData.substr(0,4)
-    setData({ ...data, date:
-        {day: day,
-        month: month,
-        year: year
-    } 
-})}
+  const dateTrim = (e) => {
+    const selectedData = e.target.value.toString();
+    const day = selectedData.substr(8, 2);
+    const month = selectedData.substr(5, 2);
+    const year = selectedData.substr(0, 4);
+    setData({ ...data, date: { day: day, month: month, year: year } });
+  };
 
-const openModal = () => {
+  const openModal = () => {
     setData({
-        type:'-',
-        category:'Other expenses', 
-        date:{
-            day: getDay(),
-            month: getMonth(),
-            year: getDefYear()
-        }})
-    setModal(true)
-}
+      type: "-",
+      category: "Other expenses",
+      date: {
+        day: getDay(),
+        month: getMonth(),
+        year: getDefYear(),
+      },
+    });
+    setModal(true);
+  };
 
-const closeModal = (e) => {
-    e.preventDefault()
-    setData({})
-    setModal(false)
-}
+  const closeModal = (e) => {
+    e.preventDefault();
+    setData({});
+    setModal(false);
+  };
 
-const sliderTypePlus = () => setData({...data, type:'+', category:'Income'})
-const sliderTypeMinus = () => setData({...data, type:'-'})
+  const sliderTypePlus = () =>
+    setData({ ...data, type: "+", category: "Income" });
+  const sliderTypeMinus = () => setData({ ...data, type: "-" });
 
-const submitModal = async (e) => {
-    e.preventDefault()
-    setModal(!modal)
-    console.log('Dane przeslane do bazy danych')
-    console.log(JSON.stringify(data))
-    
-    await fetch('https://cosmic-answer-399520.lm.r.appspot.com/api/mockTransactions/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+  const submitModal = async (e) => {
+    e.preventDefault();
+    setModal(!modal);
+    console.log("Dane przeslane do bazy danych");
+    console.log(JSON.stringify(data));
+
+    await fetch(
+      "https://cosmic-answer-399520.lm.r.appspot.com/api/mockTransactions/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
-        })
-        .then(res => res.json())
-        .then(json => setData(json.data))
-        console.log(data)
-        dataFetch();
-        console.log('dane przeladowane')
-    }
-
-
-
-    return (
-            <>
-            <button
-            onClick={openModal} 
-            className={styles.addButton}>
-            </button>
-            {modal && <ModalAddTransaction 
-            onSubmit={submitModal} 
-            onCancel={closeModal} 
-            onClose={closeModal}
-            onChangeSliderPlus={sliderTypePlus}
-            onChangeSliderMinus={sliderTypeMinus}
-            onChangeValue={e => setData({ ...data, sum: e.target.value })}
-            onChangeDate={dateTrim}
-            onChangeComment={e => setData({ ...data, comment: e.target.value })}
-            onChangeCategory={e => setData({ ...data, category: e })}
-            />}
-            </>
+      }
     )
-}
+      .then((res) => res.json())
+      .then((json) => setData(json.data));
+    console.log(data);
+    dataFetch();
+    console.log("dane przeladowane");
+  };
+
+  return (
+    <>
+      <button onClick={openModal} className={styles.addButton}></button>
+      {modal && (
+        <ModalAddTransaction
+          onSubmit={submitModal}
+          onCancel={closeModal}
+          onClose={closeModal}
+          onChangeSliderPlus={sliderTypePlus}
+          onChangeSliderMinus={sliderTypeMinus}
+          onChangeValue={(e) => setData({ ...data, sum: e.target.value })}
+          onChangeDate={dateTrim}
+          onChangeComment={(e) => setData({ ...data, comment: e.target.value })}
+          onChangeCategory={(e) => setData({ ...data, category: e })}
+        />
+      )}
+    </>
+  );
+};
--- src/components/SelectStatisticsMenu/SelectStatisticsMenu.jsx
+++ src/components/SelectStatisticsMenu/SelectStatisticsMenu.jsx
@@ -1,124 +1,129 @@
-import React, {useEffect, useState} from "react";
-import styles from "../SelectStatisticsMenu/SelectStatisticsMenu.module.css"
+import React, { useEffect, useState } from "react";
+import styles from "../SelectStatisticsMenu/SelectStatisticsMenu.module.css";
 import { nanoid } from "nanoid";
 
-const dbURL = "https://cosmic-answer-399520.lm.r.appspot.com/api/mockTransactions/?month=&year="
-
+const dbURL =
+  "https://cosmic-answer-399520.lm.r.appspot.com/api/mockTransactions/?month=&year=";
 
 export const SelectStatisticsMenu = ({ placeholder, type }) => {
+  const [data, setData] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [name, setName] = useState(`${placeholder}`);
 
-    const [data, setData] = useState([])
-    const [modal, setModal] = useState(false)
-    const [name, setName] = useState(`${placeholder}`)
+  const dataFetch = async () => {
+    const results = await (await fetch(dbURL)).json();
+    const data = results.data;
+    setData(data);
+    return data;
+  };
+  useEffect(() => {
+    dataFetch();
+  }, []);
 
-    
-    const dataFetch = async () => {
-        const results = await (await fetch(dbURL)).json();
-        const data = results.data
-        setData(data)
-        return data
-    }
-    useEffect(() => {
-        dataFetch();
-    }, []);
-                
-    const getYear = () =>{
-        const allData = []
-        data.map(({date})=>{allData.push(date.year)})
-        const sortData = [... new Set(allData)].sort()
-        const dataYear = []
-        sortData.map((date)=>{dataYear.push({'date':`${date}`},)})
-        return dataYear
-    }
+  const getYear = () => {
+    const allData = [];
+    data.map(({ date }) => {
+      allData.push(date.year);
+    });
+    const sortData = [...new Set(allData)].sort();
+    const dataYear = [];
+    sortData.map((date) => {
+      dataYear.push({ date: `${date}` });
+    });
+    return dataYear;
+  };
 
-    const getMonth = () =>{
-        const allData = []
-        data.map(({date})=>{allData.push(date.month)})
-        const sortData = [... new Set(allData)].sort()
-        const dataMonth = []
-        sortData.map((date)=>{dataMonth.push({'date':`${date}`},)})
-        return dataMonth
-    }
-    const dataYear = (getYear())
-    const dataMonth = (getMonth())
+  const getMonth = () => {
+    const allData = [];
+    data.map(({ date }) => {
+      allData.push(date.month);
+    });
+    const sortData = [...new Set(allData)].sort();
+    const dataMonth = [];
+    sortData.map((date) => {
+      dataMonth.push({ date: `${date}` });
+    });
+    return dataMonth;
+  };
+  const dataYear = getYear();
+  const dataMonth = getMonth();
 
-    const toogleModal = () => {
-        setModal(!modal)
-    }
-    const changeName = (e) => {
-        const newName = e.innerText
-        setName(newName)
-        setModal(!modal)
-        return newName
-    }
+  const toogleModal = () => {
+    setModal(!modal);
+  };
+  const changeName = (e) => {
+    const newName = e.innerText;
+    setName(newName);
+    setModal(!modal);
+    return newName;
+  };
 
-    return (
-        <>
-        <div className={styles.wrapper}>
-            <div onClick={toogleModal} className={styles.selectBtn}>
-                <span>{name}</span>
-            </div>
-        {modal&&
-            <div className={styles.optionsContainer}>
-                <ul className={styles.options}>
-                {
-                type==='year' && dataYear.length>0?
-                dataYear.map(({date})=>{
-                    return(
-                    <li key={nanoid()} 
-                    onClick={(e)=>changeName(e.target)} 
-                    className={styles.option}>
-                        <span>
-                            {date}
-                        </span>
-                    </li>
-                )})
-                : type==='month' && dataMonth.length>0?
-                dataMonth.map(({date})=>{
-                    const miesiace = new Array(
-                        "0",
-                        "January", 
-                        "February", 
-                        "March", 
-                        "April", 
-                        "May", 
-                        "June", 
-                        "July", 
-                        "August", 
-                        "September", 
-                        "October", 
-                        "November", 
-                        "December");
-                    return(
-                    <li key={nanoid()} 
-                    onClick={(e)=>changeName(e.target)} 
-                    className={styles.option}>
-                        <span>
-                            {miesiace[parseInt(date)]}
-                        </span>
-                    </li>
-                )})
-                :dataMonth==[]?
-                <li key={nanoid()} 
-                className={styles.option}>
-                    <span className={styles.nodata}>No data.</span>
-                </li>
-                :dataYear==[]?
-                <li key={nanoid()} 
-                className={styles.option}>
-                    <span className={styles.nodata}>No data.</span>
-                </li>
-                :<li key={nanoid()} 
-                className={styles.option}>
-                    <span className={styles.nodata}>No data.</span>
-                </li>
-                }
-                </ul>
-            </div>
-        }
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div onClick={toogleModal} className={styles.selectBtn}>
+          <span>{name}</span>
         </div>
-        {modal&&<div className={styles.backdrop} onClick={toogleModal}></div>}
-        </>
-    )
-}
-
+        {modal && (
+          <div className={styles.optionsContainer}>
+            <ul className={styles.options}>
+              {type === "year" && dataYear.length > 0 ? (
+                dataYear.map(({ date }) => {
+                  return (
+                    <li
+                      key={nanoid()}
+                      onClick={(e) => changeName(e.target)}
+                      className={styles.option}
+                    >
+                      <span>{date}</span>
+                    </li>
+                  );
+                })
+              ) : type === "month" && dataMonth.length > 0 ? (
+                dataMonth.map(({ date }) => {
+                  const miesiace = new Array(
+                    "0",
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                    "August",
+                    "September",
+                    "October",
+                    "November",
+                    "December"
+                  );
+                  return (
+                    <li
+                      key={nanoid()}
+                      onClick={(e) => changeName(e.target)}
+                      className={styles.option}
+                    >
+                      <span>{miesiace[parseInt(date)]}</span>
+                    </li>
+                  );
+                })
+              ) : dataMonth == [] ? (
+                <li key={nanoid()} className={styles.option}>
+                  <span className={styles.nodata}>No data.</span>
+                </li>
+              ) : dataYear == [] ? (
+                <li key={nanoid()} className={styles.option}>
+                  <span className={styles.nodata}>No data.</span>
+                </li>
+              ) : (
+                <li key={nanoid()} className={styles.option}>
+                  <span className={styles.nodata}>No data.</span>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
+      {modal && <div className={styles.backdrop} onClick={toogleModal}></div>}
+    </>
+  );
+};
